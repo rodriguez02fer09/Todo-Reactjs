942eadd60310e24547bb9038a5c0194501030b47
Fernanda Rodriguez <fer@Fernandas-MacBook-Pro.local>
2023-10-19 09:21:40 -0500

    configuracion proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,12 @@
-import {useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Button from './components/button/index'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div></div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <Button />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ src/components/button/src/Button.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import '../styles/desktop.scss'
+
+const Button = () => {
+  return <button type="button"></button>
+}
+export default Button
+s
